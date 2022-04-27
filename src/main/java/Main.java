3151a2a0f06075126306a76ca4ad76dd23ffdc49
BairamovM <muslimbairamov@gmail.com>
2022-04-27 23:40:56 +0300

    Add Log4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    public static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        LOGGER.info("Hello World");
     }
 
 }
