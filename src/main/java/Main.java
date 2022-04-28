94a077b24ae02799741d5a9364f895bf5e9d4885
BairamovM <muslimbairamov@gmail.com>
2022-04-28 20:20:41 +0300

    Add JUnit4 to the project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import org.apache.log4j.Logger;
 
 public class Main {
 
-    public static final Logger LOGGER = Logger.getLogger(Main.class);
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
 
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testCalc() {
+
+        int a = 2;
+        Assert.assertEquals("Numbers not equals", 2,a);
+
+    }
+
+}
